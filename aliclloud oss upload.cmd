8f44de4045dfc58ee586b25acde6886c2b63888e
wjgb1010 <wjgb1010@gmail.com>
2021-03-02 13:54:51 -0500

    config mercedes front

--- aliclloud oss upload.cmd
+++
@@ -1,3 +0,0 @@
-./ossutil64 cp -r dist/tienda-angular-b2b oss://sinkoola/ -u -c ~/ossutil64config
-aliyun cdn RefreshObjectCaches --ObjectType Directory --ObjectPath https://www.sinkoola.com/
-pause
