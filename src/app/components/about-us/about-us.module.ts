11609db7b9a5711758abfef2a2862add523a361b
Jose Alejandro Concepcion Alvarez <jalejandro2928@gmail.com>
2021-06-03 11:36:27 -0400

    Adding to about us dinamically and the reproduce video automatic

--- src/app/components/about-us/about-us.module.ts
+++ src/app/components/about-us/about-us.module.ts
@@ -1,21 +1,20 @@
+import { PipesModule } from './../../core/pipes/pipes.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    AboutUsComponent
-  ],
+  declarations: [AboutUsComponent],
   imports: [
     CommonModule,
     AboutUsRoutingModule,
@@ -27,7 +26,9 @@ import {MatListModule} from '@angular/material/list';
     MatInputModule,
     TranslateModule,
     MatListModule,
-    MatIconModule,MatButtonModule
-  ]
+    MatIconModule,
+    MatButtonModule,
+    PipesModule,
+  ],
 })
-export class AboutUsModule { }
+export class AboutUsModule {}
--- src/app/core/services/contact-us/contact-us.service.ts
+++ src/app/core/services/contact-us/contact-us.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -18,45 +19,11 @@ export class ContactUsService {
     return this.httpClient.post<any>(this.url, body);
   }
 
-  editContactUs(data) {
-    return this.httpClient.patch<any>(this.urlId.replace(':id', data.id), data, this.httpOptions);
-  }
-
-  removeContactUs(data): Promise<any> {
-    return this.httpClient.delete<any>(this.urlId.replace(':id', data.id), this.httpOptions).toPromise();
-  }
-
-  getAllMessages(query?: IPagination, params?: any) {
-    let httpParams = new HttpParams();
-    if (query) {
-      httpParams = httpParams.append('limit', query.limit.toString());
-      httpParams = httpParams.append('offset', query.offset.toString());
-
-      if (query.filter && query.filter.properties) {
-        query.filter.properties.forEach((item) => {
-          httpParams = httpParams.append(item, '%' + query.filter.filterText + '%');
-        });
-      }
-
-      if (query.order) {
-        httpParams = httpParams.append('order', query.order);
-      }
-    } else {
-      httpParams = httpParams.set('limit', '0');
-      httpParams = httpParams.set('offset', '0');
-    }
-    if (params) {
-    }
-    return this.httpClient.get<any>(this.url, { params: httpParams });
-  }
-
-  getContactUs(data: any): Observable<any> {
-    console.log('TCL: ContactUsService -> constructor -> data', data);
-
-    if (data.constructor === Object) {
-      return this.httpClient.get<any>(this.urlId.replace(':id', data.id), this.httpOptions);
-    } else {
-      return this.httpClient.get<any>(this.urlId.replace(':id', data + ''), this.httpOptions);
-    }
+  getAboutUs(): Observable<any> {
+    return this.httpClient.get<any>(environment.apiUrl + 'about-us').pipe(
+      map((data) => {
+        return data?.data[0];
+      }),
+    );
   }
 }
