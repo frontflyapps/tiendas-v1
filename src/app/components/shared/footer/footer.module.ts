30d1e1b53396a93ab9cfdf7916fac45be5c26bbd
DmNVlop <DmNVlop@gmail.com>
2021-08-13 00:04:29 -0400

    footer y meta index

---
+++ src/app/components/shared/footer/footer.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatSelectModule} from '@angular/material/select';
+import {FooterComponent} from './footer.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+    declarations: [FooterComponent],
+    exports: [FooterComponent],
+  imports: [
+    MatSelectModule,
+    CommonModule,
+    TranslateModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatGridListModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    RouterModule,
+    MatMenuModule,
+  ],
+})
+export class FooterModule {
+}
