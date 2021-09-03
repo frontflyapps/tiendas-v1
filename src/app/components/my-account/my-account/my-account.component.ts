cd03ddd14712e49460bcd634a100f5574d21903b
DmNVlop <DmNVlop@gmail.com>
2021-09-03 13:40:12 -0400

    correction username visible

--- src/app/components/my-account/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account/my-account.component.ts
@@ -9,7 +9,7 @@ import { ShowSnackbarService } from '../../../core/services/show-snackbar/show-s
 import { environment } from '../../../../environments/environment';
 import { AuthenticationService } from '../../../core/services/authentication/authentication.service';
 import { LoggedInUserService } from '../../../core/services/loggedInUser/logged-in-user.service';
-import { CUBAN_PHONE_START_5, EMAIL_REGEX } from '../../../core/classes/regex.const';
+import { CHARACTERS_AND_NUMBERS, CUBAN_PHONE_START_5, EMAIL_REGEX } from '../../../core/classes/regex.const';
 
 @Component({
   selector: 'app-my-account',
@@ -146,7 +146,10 @@ export class MyAccountComponent implements OnInit {
     this.registrationForm = this.fb.group({
       name: [null, [Validators.required, Validators.pattern(/^\w((?!\s{2}).)*/)]],
       lastname: [null, [Validators.required, Validators.pattern(/^\w((?!\s{2}).)*/)]],
-      username: [null],
+      username: [null, [
+        Validators.required,
+        Validators.pattern(CHARACTERS_AND_NUMBERS),
+      ]],
       phone: [null, [
         Validators.pattern(CUBAN_PHONE_START_5),
         Validators.minLength(8),
