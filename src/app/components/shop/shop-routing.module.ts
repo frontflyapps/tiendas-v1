5b056eb88282e1274a2cc678e6eef4cb3b2767ef
DmNVlop <DmNVlop@gmail.com>
2021-09-16 16:58:41 -0400

    minor

--- src/app/components/shop/shop-routing.module.ts
+++ src/app/components/shop/shop-routing.module.ts
@@ -3,9 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductDetailsComponent } from './products/product-details/product-details.component';
 import { ProductLeftSidebarComponent } from './products/product-left-sidebar/product-left-sidebar.component';
 import { MainHomeComponent } from './main-home/main-home.component';
-// import { HomeTwoComponent } from './home-two/home-two.component';
-// import { HomeThreeComponent } from './home-three/home-three.component';
-// import { HomeFiveComponent } from './home-five/home-five.component';
 
 // Routes
 const routes: Routes = [
