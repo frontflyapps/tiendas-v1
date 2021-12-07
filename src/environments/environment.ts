f3df888e3316855b8658744a3f15d43efc07b95b
DmNVlop <DmNVlop@gmail.com>
2021-12-07 11:00:14 -0500

    minor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,11 @@ export const environment = {
   // apiUrl: 'http://192.168.1.17:8998/v1/',
   // imageUrl: 'http://192.168.1.17:8998/v1/',
 
-  // apiUrl: 'http://192.168.43.231:8998/v1/',
-  // imageUrl: 'http://192.168.43.231:8998/v1/',
+  apiUrl: 'http://127.0.0.1:8998/v1/',
+  imageUrl: 'http://127.0.0.1:8998/v1/',
 
-  apiUrl: 'https://apitienda.marinasmarlin.com/v1/',
-  imageUrl: 'https://apitienda.marinasmarlin.com/v1/',
+  // apiUrl: 'https://apitienda.marinasmarlin.com/v1/',
+  // imageUrl: 'https://apitienda.marinasmarlin.com/v1/',
 
   // apiUrl: 'https://api.mibulevar.com/v1/',
   // imageUrl: 'https://api.mibulevar.com/v1/',
