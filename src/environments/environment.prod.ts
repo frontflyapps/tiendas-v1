d281b2915dae01ddf6cf337e815606d30a7e5f84
carlos <carlosasg981212@gmail.com>
2023-05-05 16:27:40 -0400

    dev identity

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,15 +3,14 @@ export const PASARELA_BASE = 'peoplegoto';
 export const environment = {
   production: true,
 
-  apiUrl: 'https://api.mibulevar.com/v1/',
-  imageUrl: 'https://cdn.mibulevar.com/tienda/',
+  apiUrl: 'https://api.guajiritos.com/v1/',
+  imageUrl: 'https://cdntienda.guajiritos.com/',
 
   defaultLanguage: 'es',
   currencyInternational: 'USD',
-
-  logo: 'assets/images/logo-navbar.png',
-  logoWhite: 'assets/images/logo-navbar.png',
-  logoFooter: 'assets/images/logo-navbar.png',
+  logo: 'assets/images/guajiritos/guajiritos-Logo-blanco.png',
+  logoWhite: 'assets/images/guajiritos/guajiritos-Logo-blanco.png',
+  logoFooter: 'assets/images/guajiritos/guajiritos-Logo-blanco.png',
 
   limitSearch : 42,
 
@@ -24,31 +23,31 @@ export const environment = {
   timeToResearchMenuData: 300000, // (30min) Time to research menu (ms)
   timeToResearchCategoriesData: 600000, // (10min) Time to research categories (ms)
 
-  url: 'https://cupethol.mibulevar.com',
-  urlAboutUs: 'https://cupethol.mibulevar.com',
+  url: 'https://tienda.guajiritos.com/',
+  urlAboutUs: 'https://tienda.guajiritos.com/',
 
-  address: 'UEB Division Territorial Combustible Holguín | TransferMóvil',
+  address: 'Tienda GuajiTech DEV',
   localDatabaseUsers: true,
 
   contacts: {
-    phone: '(+53) 59962832',
-    email: 'tvirtualhlg@echol.cupet.cu',
-    address: 'Calle 3 #20 /4ta y carretera Central, zona industria, Guirabito',
+    phone: '',
+    email: '',
+    address: '',
   },
 
-  adminService: 'https://admin.mibulevar.com/',
-  mainDomain: '.mibulevar.com',
+  adminService: 'https://admintienda.guajiritos.com/',
+  mainDomain: '.tienda.guajiritos.com',
 
   tokenBusiness:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IkJ1c2luZXNzSWQiOjYxLCJpZCI6NjEsImRhdGUiOiIyMDIyLTA2LTI3VDIwOjIzOjU5LjQwM1oiLCJuYW1lIjoiQ29tZXJjaW8gTWlub3Jpc3RhIENpZW5mdWVnb3MiLCJlbWFpbCI6Im1laWxpLmhlcm5uYWRlekBnZWNjLmNvLmN1In0sImlhdCI6MTY1NjM2MTQzOX0.iW0tQMfs2Y1yrz1y2Qe88ZNp84uvY3fDoFirx-TlZ7Y',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7IkJ1c2luZXNzSWQiOjEsImlkIjoxLCJkYXRlIjoiMjAyMS0wOS0xM1QwMTo0NToxOC4wMDBaIiwibmFtZSI6IlRpZW5kYSIsImVtYWlsIjoidGllbmRhQGxvY2FsaG9zdC5jb20ifSwiaWF0IjoxNjMxNDk3NTkxfQ.FF5-276rI9SJmtebVo0spV8Y2wdOZPok7LlUxtTDwh0',
 
   meta: {
     mainPage: {
-      title: 'Cupet Cienfuegos  | TransferMóvil',
+      title: 'Tienda Guajitech DEV',
       description: 'Tienda online desarrollada con el framework Angular permite la modelación de negocios B2C and C2C',
-      keywords: 'HTML, CSS, JavaScript, Angular, Tienda Online B2B, Sin Cola, comercio online',
-      shareImg: 'https://cupetcfg.mibulevar.com/assets/images/share-img.png',
-      url: 'https://cupetcfg.mibulevar.com/',
+      keywords: 'HTML, CSS, JavaScript, Angular, Tienda Online B2B, comercio online',
+      shareImg: 'https://tienda.guajiritos.com/assets/images/share-img.png',
+      url: 'https://tienda.guajiritos.com/',
     },
   },
 
