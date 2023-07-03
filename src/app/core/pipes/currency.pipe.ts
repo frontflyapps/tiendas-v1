af4e45fe220e66f1e850fe82375bbc3e06071d9a
carlos <carlosasg981212@gmail.com>
2023-07-03 10:15:21 -0400

    currency in product details, cart, shopping widget, checkout

---
+++ src/app/core/pipes/currency.pipe.ts
@@ -0,0 +1,33 @@
+import { CurrencyPipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import { CoinEnum } from '../classes/coin.enum';
+import { MarketEnum } from '../classes/market.enum';
+
+@Pipe({
+  name: 'currencyProduct',
+})
+export class CurrencyProductPipe implements PipeTransform {
+  constructor(private currencyPipe: CurrencyPipe) {
+  }
+
+  transform(price: number, currency: string): string {
+    let newCurrency;
+    switch (currency) {
+      case 'USD': {
+        newCurrency = CoinEnum.USD;
+        break;
+      }
+      case 'EUR': {
+        newCurrency = CoinEnum.EUR;
+        break;
+      }
+      case 'CUP': {
+        newCurrency = CoinEnum.CUP;
+        break;
+      }
+    }
+    const newPrice = this.currencyPipe.transform(price, currency);
+
+    return  newPrice;
+  }
+}
